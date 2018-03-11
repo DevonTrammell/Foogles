911ff66e4951d002043b65ef45fc5ef2627ead56
devon.trammell502 <devon.trammell502@myci.csuci.edu>
2018-03-11 13:48:18 -0700

    This should contain the game screens, they do not have connecting intents yet.

---
+++ Foogles/app/src/main/java/fooglesinc/foogles/foogle_swim.java
@@ -0,0 +1,13 @@
+package fooglesinc.foogles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class foogle_swim extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foogle_swim);
+    }
+}
